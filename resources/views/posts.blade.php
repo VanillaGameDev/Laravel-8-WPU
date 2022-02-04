1d7bdb075cd02c9f2dfb2d78dcf875a861178d0f
Bang Mirza <pandimirza500@gmail.com>
2022-02-04 17:24:51 +0700

    updated

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.main')
 
 @section('container')
+<h1 class="mb-4">Halaman Blog Post</h1>
+<p><a href="/categories" class="text-decoration-none">See All Category</a></p>
 
     @foreach($posts as $post)
-    <article class="mb-5">
-        <h2>
-            <a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a>
-        </h2>
+    <article class="mb-5 border-bottom pb-3">
 
-        <h5>By: {{ $post['author'] }}</h5>
-        <p>{{ $post['body'] }}</p>
+        <h2>{{ $post->title }}</h2>
+
+        <h5>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}"
+        class="text-decoration-none">{{ $post->category->name }}</a></h5>
+
+        <p>{{ $post->excerpt }}</p>
+
+        <a href="/posts/{{ $post->slug }}"class="text-decoration-none">Read More..</a>
 
     </article>
 
