1a561ec8270e5a34ebd51b0fea8044e279b43afe
Bang Mirza <pandimirza500@gmail.com>
2022-08-04 18:28:17 +0700

    updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,9 +2,17 @@
 
 @section('container')
 
-<h2>about page</h2>
-<h3>{{ $name }}</h3>
-<p>{{ $email }}</p>
-<img src="img/{{ $image }}" alt="{{ $name }}" class="img-thumbnail rounded-circle">
+    <div class="about-main">
+        <div class="img-about"></div>
+            <h1 class="h1-about">Mirza El Fandi</h1>
+            <div class="text-about">Web Developer From <strong>Indoneseia</strong></div>
+            <p class="p-about">Selamat datang di website saya, di website ini kalian bisa melihat banyak berita-berita menarik!. Saya bersekolah di Bina Mandiri Multimedia 3, Kelas 12 jurusan Rekayasa Perangkat Lunak.</p>
+            <ul class="ul-about">
+                <li class="li-about"><a href="#"><img src="img/fb.png" width="25px" height="25px"></a></li>
+                <li class="li-about"><a href="#"><img src="img/fb.png" width="25px" height="25px"></a></li>
+                <li class="li-about"><a href="#"><img src="img/fb.png" width="25px" height="25px"></a></li>
+                <li class="li-about"><a href="#"><img src="img/fb.png" width="25px" height="25px"></a></li>
+            </ul>
+    </div>
 
 @endsection
